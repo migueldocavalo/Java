634b05d39a32b317d5a25fd4b4406dd4317f89fd
miguel garcia <miguel_garcia5555@hotmail.com>
2025-02-20 20:59:38 -0300

    Create exercicio5.java

---
+++ exercicio5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IdadeParaDias {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a idade em anos: ");
+        int idade = sc.nextInt();
+
+        int diasVividos = idade * 365;
+        System.out.println("Você viveu aproximadamente " + diasVividos + " dias.");
+
+        sc.close();
+    }
+}
